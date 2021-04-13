78aa3a54bb39e340e7e32da322abad6ed2571ebc
AndreaTrento95 <trentora95@gmail.com>
2021-04-13 16:32:12 +0200

    svolgimento

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,12 +3,48 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while. */
 
 
-// chiedo all'utente di inserire un numero per 5 volte
+
+// ---------- FOR -------------
+// chiedo all'utente di inserire un numero per 5 volte, il risulatto lo metto dentro un array
 var arr = [];
 
 for(var i = 0; i < 5; i++){
     var numero_utente = parseInt(prompt("Inserisci un numero"));
     arr.push(numero_utente);
 }
-console.log(arr);
+
+// creo un ciclo per prendere i singoli elementi degli array e sommarli
+var somma = 0;
+
+for(var c = 0; c < arr.length; c++){
+    elemento = arr[c];
+    somma += elemento ;
+}
+
+// output
+console.log(somma);
+
+
+
+// ---------- WHILE ---------
+var arr = [];
+
+var counter = 0;
+while(counter < 5){
+    var numero_utente = parseInt(prompt("Inserisci un numero"));
+    arr.push(numero_utente);
+    counter++ ;
+}
+
+
+var somma = 0;
+var i = 0;
+while(i < arr.length){
+    var elemento = arr[i];
+    somma += elemento;
+    i++ ;
+}
+console.log(somma);
+
+
 
